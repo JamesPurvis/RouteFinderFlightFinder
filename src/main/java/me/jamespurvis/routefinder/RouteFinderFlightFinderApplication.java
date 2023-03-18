60f3084d993b28af1ec506cb7adb30072afb7e6d
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-03-18 13:52:26 -0600

    intial commit

---
+++ src/main/java/me/jamespurvis/routefinder/RouteFinderFlightFinderApplication.java
@@ -0,0 +1,13 @@
+package me.jamespurvis.routefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteFinderFlightFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteFinderFlightFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jamespurvis/routefinder/RouteFinderFlightFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jamespurvis.routefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteFinderFlightFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
